5986f8893d54aa22b0af1f6b0f6ba9cf7ddf61d1
Yagmurtascii <yagmur_tascii@hotmail.com>
2024-04-02 01:21:46 +0300

    Animation, Sounds and Password
    door animation added.
    switch animation added.
    door open, switch open, locked sound added.
    Password Manager Added and created panel for password screen.

--- Assets/Scripts/CrossHairController.cs
+++ Assets/Scripts/CrossHairController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
@@ -7,15 +8,45 @@ using UnityEngine.UI;
 
 public class CrossHairController : MonoBehaviour
 {
+    [Header("Camera")]
     public Camera playerCamera; // Oyuncu kameranýzý atayýn
+    public Camera passwordCamera; // Oyuncu kameranýzý atayýn
+
+    [Header("UI")]
     public Text text;
-    private List<GameObject> lamps = new List<GameObject>();
-    public Light[] lights;
+    public GameObject panel;
+
+
+    [Header("Boolean/Float")]
     public bool isActiveDoor = false;
     public bool isOpen = false;
+    public bool isLightOpen = false;
+    public bool ispassword = false;
+    public bool isPanel = false;
+    public float distance = 0f;
+
+    [Header("Animations")]
     [SerializeField]
     private Animator doorAnimator;
-    public float distance=0f;
+    [SerializeField]
+    private Animator switchAnimator;
+
+
+    [Header("Lights")]
+    [SerializeField]
+    private KeepLights lights;
+
+    [Header("Sounds")]
+    [SerializeField]
+    private AudioSource doorOpenSource;
+    [SerializeField]
+    private AudioSource backDoorSoundSource;
+    [SerializeField]
+    private AudioSource lightSoundSource;
+
+    public PasswordManage passwordManage;
+
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // Fare týklamasý algýlandýðýnda
@@ -27,32 +58,105 @@ public class CrossHairController : MonoBehaviour
             {
                 GameObject obj = hit.collider.gameObject; // Çarpýlan objeyi al
                 text.text = obj.tag;
-
+                passwordCamera.enabled = false;
                 Debug.Log("Baktýðýnýz obje: " + obj.name);
-                 distance = Vector3.Distance(obj.transform.position, gameObject.transform.position);
-                if (obj.tag == "Door" && distance<20f)
+                distance = Vector3.Distance(obj.transform.position, gameObject.transform.position);
+                if (obj.tag == "Door" && distance < 20f)
                 {
-                    isActiveDoor = true;
-                    
                     doorAnimator = obj.GetComponent<Animator>();
-                    if (isActiveDoor && !isOpen)
+
+                    if (!isOpen)
                     {
                         obj.GetComponent<BoxCollider>().isTrigger = true;
-                        doorAnimator.SetBool("isOpen", true);
+                        doorAnimator.SetTrigger("isTrigger");
                         isOpen = true;
+                        doorOpenSource.Play();
+
                     }
-                    else if (isActiveDoor && isOpen) {
+                    else if (isOpen)
+                    {
 
                         obj.GetComponent<BoxCollider>().isTrigger = false;
-                        doorAnimator.SetBool("isOpen", false);
+                        doorAnimator.SetTrigger("isTrigger");
                         isOpen = false;
+                        doorOpenSource.Play();
+
                     }
-                   
+
+                }
+                else if (obj.tag == "BackDoor")
+                {
+                    backDoorSoundSource.Play();
+                }
+                else if (obj.tag == "Switch")
+                {
+
+                    lights = obj.GetComponent<KeepLights>();
+
+                    switchAnimator = obj.GetComponent<Animator>();
+                    if (!isLightOpen)
+                    {
+                        lightSoundSource.Play();
+                        switchAnimator.SetTrigger("isTrigger");
+                        for (int i = 0; i < lights.lights.Length; i++)
+                        {
+                            lights.lights[i].enabled = true;
+                        }
+
+                        isLightOpen = true;
+                    }
+                    else
+                    {
+                        lightSoundSource.Play();
+                        switchAnimator.SetTrigger("isTrigger");
+                        for (int i = 0; i < lights.lights.Length; i++)
+                        {
+                            lights.lights[i].enabled = false;
+                        }
+                        isLightOpen = false;
+                    }
+
+
+                }
+                else if (obj.tag == "DoorLock")
+                {
+                    panel.SetActive(true);
+                    Time.timeScale = 0f;
+                    Cursor.lockState = CursorLockMode.None;
+                }
+                else if (obj.tag == "PasswordDoor")
+                {
+
+                    doorAnimator = obj.GetComponent<Animator>();
+
+                    if (passwordManage.isOpenDoor)
+                    {
+                        if (!isOpen)
+                        {
+                            obj.GetComponent<BoxCollider>().isTrigger = true;
+                            doorAnimator.SetTrigger("isTrigger");
+                            isOpen = true;
+                            doorOpenSource.Play();
+
+                        }
+                        else if (isOpen)
+                        {
+
+                            obj.GetComponent<BoxCollider>().isTrigger = false;
+                            doorAnimator.SetTrigger("isTrigger");
+                            isOpen = false;
+                            doorOpenSource.Play();
+
+                        }
+                    }
+                    else
+                        backDoorSoundSource.Play();
+
                 }
             }
 
         }
     }
- 
+
 }
 
